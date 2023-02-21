471df7c4ffb0f7261a29ba0c46e103a8236a8d4a
AnnaJever <65163970+AnnaJever@users.noreply.github.com>
2023-02-21 14:25:34 +0100

    Updated code and data.

--- pollen_analysis.R
+++ pollen_analysis.R
@@ -1,8 +1,9 @@
 ### Analysis of manuscript with title:
 
-# Palaeoecological data reveal long-term trajectories of non-native vegetation on islands globally
+# Long-term trajectories of non-native vegetation on islands globally
 # Walentowitz et al.
-# last edited on 16. November 2022
+# last edited on 21. February 2023
+# Ecology Letters 2023
 
 
 
@@ -44,7 +45,7 @@ rownames(box_table_upper) <- env$island
 
 
 
-### data preparation and single island analsis
+### data preparation and single island analysis
 
 for(i in 1:nrow(env)){
   if(!is.na(env$pollen_assign[i]) &&
@@ -117,10 +118,10 @@ for(i in 1:nrow(env)){
     pollen_df <- as.data.frame(as.numeric(ages[[paste0(env$ID[i])]]))
     colnames(pollen_df) <- c("time")
     
-    pollen_df$sum.inv.lower <- round(rowSums(pollen_lower[which(colnames(pollen_lower) %in% ali)]))
-    pollen_df$sum.inv.upper <- round(rowSums(pollen_upper[which(colnames(pollen_upper) %in% ali)]))
-    pollen_df$sum.endem     <- round(rowSums(pollen_raw[which(colnames(pollen_raw) %in% endemic)]))
-    pollen_df$sum.crops     <- round(rowSums(pollen_raw[which(colnames(pollen_raw) %in% crop)]))
+    pollen_df$sum.inv.lower <- round(rowSums(pollen_lower[which(colnames(pollen_lower) %in% ali)]), digits = 2)
+    pollen_df$sum.inv.upper <- round(rowSums(pollen_upper[which(colnames(pollen_upper) %in% ali)]), digits = 2)
+    pollen_df$sum.endem     <- round(rowSums(pollen_raw[which(colnames(pollen_raw) %in% endemic)]), digits = 2)
+    pollen_df$sum.crops     <- round(rowSums(pollen_raw[which(colnames(pollen_raw) %in% crop)]), digits = 2)
     
 
     # number of pollen taxa
@@ -136,7 +137,6 @@ for(i in 1:nrow(env)){
     
     # final dataset cut off data at 5000 years before now
     pollen_df <- pollen_df[order(pollen_df$time, decreasing = T),]
-    pollen_df[,c(2:5)] <- round(pollen_df[,c(2:5)])
     pollen_df <- pollen_df[complete.cases(pollen_df),]
     pollen_df <- pollen_df[pollen_df$time <= 5000,]
     
@@ -144,7 +144,7 @@ for(i in 1:nrow(env)){
   
     ### Single island figure
     # Figures with moving averages
-    # Take the widow size so that ~ 25 averages are being made.
+    # Select window size so that ~ 25 averages are being made.
     # This means that each average covers about 200 years.
     # Additional condition: at least 2 data points must be averaged
     # k is thus individual for each island
@@ -186,7 +186,7 @@ for(i in 1:nrow(env)){
       filter(category == "a")
     
     # Additional line for moving average
-    # vertial black line indicating human arrival
+    # vertical black line indicating human arrival
     plot_mw_lower <- ggplot(pollen_df, aes(time, sum.inv.lower)) +
       geom_point(aes(time,sum.inv.lower), col = "white")+
       geom_point(data = pollen_df_a, aes(time,sum.inv.lower), col = "salmon") + 
@@ -204,10 +204,7 @@ for(i in 1:nrow(env)){
       theme(axis.title.x=element_blank(),
             axis.title.y=element_blank())
 
-    svg(file=paste0("Figures/trend/lower_trend_", env$island[i], ".svg"),
-        height = 2, width = 1.7)
-    print(plot_mw_lower)
-    dev.off()
+    plot_mw_lower
     
     
     
@@ -228,10 +225,7 @@ for(i in 1:nrow(env)){
       theme(axis.title.x=element_blank(),
             axis.title.y=element_blank())
     
-    svg(file=paste0("Figures/trend/upper_trend_", env$island[i], ".svg"),
-        height = 2, width = 1.7)
-    print(plot_mw_upper)
-    dev.off() 
+    plot_mw_upper 
 
 
     
@@ -263,10 +257,7 @@ for(i in 1:nrow(env)){
             axis.ticks.y = element_blank(),
             axis.text.y = element_blank())
     
-      svg(file=paste0("Figures/trend/lower_boxp_", env$island[i], ".svg"),
-          height = 2, width = 0.7)
-      print(boxplot_lower)
-      dev.off()
+    boxplot_lower
       
 
       box_table_lower$island[i] <- env$island[i]
@@ -297,10 +288,7 @@ for(i in 1:nrow(env)){
               axis.ticks.y = element_blank(),
               axis.text.y = element_blank())
       
-      svg(file=paste0("Figures/trend/upper_boxp_", env$island[i], ".svg"),
-          height = 2, width = 0.7)
-      print(boxplot_upper)
-      dev.off()    
+      boxplot_upper   
       
       
     box_table_upper$island[i] <- env$island[i]
@@ -367,7 +355,7 @@ mean(box_table_upper$end_value_ali) #25%
 # LMM
 # build LMM to explain sum of non-native pollen with time and with islands as random effect
 
-# remove Foa and Ha'afeva from multi-island analysis. The pollen data had a very low temporal distribution
+# remove Foa and Ha'afeva from multi-island analysis as the pollen data had a very low temporal distribution
 
 pollen_all <- pollen_all[which(pollen_all$island != c("Ha'afeva", "Foa")),]
 
@@ -387,7 +375,6 @@ all_pollen_glmm1b <- lmer(log1p(sum.inv.upper) ~ time + (1|island), data = polle
   rc_resids1a <- compute_redres(all_pollen_glmm1a)
   hist(rc_resids1a)
   table.nonnata <- as.data.frame(report_table(all_pollen_glmm1a))
-  write.csv2(table.nonnata, "table.nonnat_lower.csv")
 
   print(all_pollen_glmm1b, corr=F)
   summary(all_pollen_glmm1b)
@@ -396,7 +383,6 @@ all_pollen_glmm1b <- lmer(log1p(sum.inv.upper) ~ time + (1|island), data = polle
   rc_resids1b <- compute_redres(all_pollen_glmm1b)
   hist(rc_resids1b)
   table.nonnatb <- as.data.frame(report_table(all_pollen_glmm1b))
-  write.csv2(table.nonnatb, "table.nonnat_upper.csv")
 
 
 
@@ -405,7 +391,7 @@ all_pollen_glmm1b <- lmer(log1p(sum.inv.upper) ~ time + (1|island), data = polle
   
 pollen_2000 <- pollen_all[which(pollen_all$time <= 2000),]
 
-#fit simple linear regression model
+# fit simple linear regression model
 
 fit1 <- lm(sum.inv.lower ~ time, data=pollen_2000)
 fit2 <- lm(sum.inv.upper ~ time, data=pollen_2000)
@@ -421,19 +407,17 @@ dat2 = data.frame(x = pollen_2000$time, y = broken.line(segmented.fit2)$fit)
 summary(segmented.fit1) # 575 
 plot(segmented.fit1)
 
-summary(segmented.fit2) # 280 
+summary(segmented.fit2) # 254 
 plot(segmented.fit2)
 
 table.nonnat_piecwise1 <- as.data.frame(report_table(segmented.fit1))
-write.csv2(table.nonnat_piecwise1, "table.nonnat_lower.csv")
 
 table.nonnat_piecwise2 <- as.data.frame(report_table(segmented.fit2))
-write.csv2(table.nonnat_piecwise2, "table.nonnat_upper.csv")
 
 mean(pollen_all$sum.inv.lower[which(pollen_all$time < 575)])# 5%
 mean(pollen_all$sum.inv.lower[which(pollen_all$time > 575)])# 2%
 
-mean(pollen_all$sum.inv.upper[which(pollen_all$time < 280)])# 20%
+mean(pollen_all$sum.inv.upper[which(pollen_all$time < 280)])# 19%
 mean(pollen_all$sum.inv.upper[which(pollen_all$time > 280)])# 7%
 
 
@@ -458,9 +442,8 @@ rc_resids2 <- compute_redres(all_pollen_glmm2)
 hist(rc_resids2)
 
 table.endemics <- as.data.frame(report_table(all_pollen_glmm2)) 
-write.csv2(table.endemics, "table.endemics.csv")
 
-# model cultivars
+# lmm cultivars
 all_pollen_glmm3 <- lmer(log1p(sum.crops) ~ time + (1|island), data = pollen_all)
 
 print(all_pollen_glmm3, corr=F)
@@ -471,12 +454,11 @@ rc_resids3 <- compute_redres(all_pollen_glmm3)
 hist(rc_resids3)
 
 table.cultivars <- as.data.frame(report_table(all_pollen_glmm3)) 
-write.csv2(table.cultivars, "table.cultivars.csv")
 
 
 
 ### multi-island plots
-# plot pollen abundance 5000 years (affinity 300*300)
+# plot pollen abundance 5000 years
 plot_abu <- ggplot(pollen_all) +
   geom_point(aes(time,sum.inv.lower), col = "indianred4", alpha = 0.3, size = 0.5) +
   geom_point(aes(time,sum.inv.upper), col = "salmon", alpha = 0.3, size = 0.5) +
@@ -487,21 +469,21 @@ plot_abu <- ggplot(pollen_all) +
   theme(panel.background = element_rect(fill = "NA", colour = "black"))+
   theme(axis.text = element_text(size = 12))+
   theme(axis.title = element_text(size = 12))+
-  labs(x = "Time [Cal. years BP]", y = "Pollen [%]")+
+  labs(x = "Time [cal. years BP]", y = "Pollen [%]")+
   ylim(0,50)+
   xlim(5000,-70)
 
 plot_abu
 
 
-# breakpoint (affinity 300*300)
+# breakpoint
 plot_bp <- ggplot(pollen_all) +
   geom_point(aes(time,sum.inv.lower), col = "indianred4", alpha = 0.3, size = 0.5) +
   geom_point(aes(time,sum.inv.upper), col = "salmon", alpha = 0.3, size = 0.5) +
   geom_line(aes(x = x, y = y), data = dat2, color = 'salmon', size = 2)+
   geom_line(aes(x = x, y = y), data = dat1, color = 'indianred4', size = 2)+
   theme(panel.background = element_rect(fill = "NA", colour = "black"))+
-  labs(x = "Time [Cal. years BP]", y = "Non-native pollen [%]")+
+  labs(x = "Time [cal. years BP]", y = "Non-native pollen [%]")+
   theme(axis.text = element_text(size = 12))+
   theme(axis.title = element_text(size = 12))+
   xlim(2000,-70)+
@@ -511,7 +493,7 @@ plot_bp
 
 
 
-# corr species and abundance (affinity 300*300)
+# corr species and abundance
 plot_cor.lower <- ggplot(pollen_all) +
   geom_point(aes(sum.inv.upper,sp.ali.lower), col = "salmon")+
   geom_smooth(aes(sum.inv.upper,sp.ali.lower), method = "lm", col = "salmon")+
@@ -535,20 +517,15 @@ plot_cor.upper
 
 
 
-# crop pollen in time
+# cultivar pollen in time
 pollen_all_crop <- pollen_all[which(pollen_all$sum.crops > 0 & !is.na(pollen_all$sum.crops)),]
-
-# build GLMM explain sum of crop pollen with time and with islands as random effect
-# extreme values in Gran Canaria. Add the following term to exclude
-# pollen_all_crop$island != "Gran Canaria"
-# plot pollen abundance 5000 years (affinity 300*300)
 
 plot_crop <- ggplot(pollen_all_crop[which(pollen_all_crop$time <= 2000),])+
   geom_point(aes(time,sum.crops), col = "salmon", alpha = 0.5) +
   geom_smooth(aes(time,sum.crops), span = 1,
               se = T, colour = "salmon")+
   theme(panel.background = element_rect(fill = "NA", colour = "black"))+
-  labs(x = "Time [Cal. years BP]", y = "Cultivar pollen [%]")+
+  labs(x = "Time [cal. years BP]", y = "Cultivar pollen [%]")+
   theme(axis.text = element_text(size = 12))+
   theme(axis.title = element_text(size = 12))+
   xlim(2000,0)
@@ -556,18 +533,14 @@ plot_crop <- ggplot(pollen_all_crop[which(pollen_all_crop$time <= 2000),])+
 plot_crop
 
 # endemic pollen in time
-pollen_all_endem <- pollen_all[which(pollen_all$sum.endem > 0 & !is.na(pollen_all$sum.crops)),]
+pollen_all_endem <- pollen_all[which(pollen_all$sum.endem > 0 & !is.na(pollen_all$sum.endem)),]
 
-# build GLMM explain sum of crop pollen with time and with islands as random effect
-# extreme values in Gran Canaria. Add the following term to exclude
-# pollen_all_crop$island != "Gran Canaria"
-# plot pollen abundance 5000 years (affinity 300*300)
 plot_endem <- ggplot(pollen_all_endem)+
   geom_point(aes(time,sum.endem), col = "darkblue", alpha = 0.5) +
   geom_smooth(aes(time,sum.endem), span = 1,
               se = T, colour = "darkblue")+
   theme(panel.background = element_rect(fill = "NA", colour = "black"))+
-  labs(x = "Time [Cal. years BP]", y = "Endemic pollen [%]")+
+  labs(x = "Time [cal. years BP]", y = "Endemic pollen [%]")+
   theme(axis.text = element_text(size = 12))+
   theme(axis.title = element_text(size = 12))+
   xlim(5000,0)
@@ -579,4 +552,4 @@ plot_endem
 
 # number of unique taxa looked at in this analysis
 length(unique(pollen_tax$tax)) # 1158
-length(unique(pollen_tax_filtered$tax_filtered)) # 652
+length(unique(pollen_tax_filtered$tax_filtered)) # 665
